e07df1acd52d60a780d2ef7ec018a481cf3fd4b0
baldore <acornssn@gmail.com>
2021-04-19 18:58:56 -0500

    Add logic to read files

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-func main() {
-	fmt.Printf("%v\n", "hola mundo genial!!!!")
+var directoryFlag string
 
-	files, err := ioutil.ReadDir(".")
+func main() {
+	flag.StringVar(&directoryFlag, "d", "", "Directory to look for files")
+	flag.Parse()
+
+	if directoryFlag == "" {
+		log.Fatal("directoryFlag is mandatory")
+	}
+
+	files, err := ioutil.ReadDir(directoryFlag)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Printf("%v\n", file.Name())
 	}
 }
