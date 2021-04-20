86de3b28a288e2a4333e70caf0ab81f0ad5e822d
baldore <acornssn@gmail.com>
2021-04-19 19:14:53 -0500

    Add filtering for epub files

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
-var directoryFlag string
-
 func main() {
+	var directoryFlag string
+
 	flag.StringVar(&directoryFlag, "d", "", "Directory to look for files")
 	flag.Parse()
 
@@ -22,7 +24,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
+	epubFiles := getFilesWithExtension(files, "epub")
+
+	for _, file := range epubFiles {
 		fmt.Printf("%v\n", file.Name())
 	}
+}
+
+func getFilesWithExtension(files []fs.FileInfo, ext string) []fs.FileInfo {
+	var filtered []fs.FileInfo
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), "."+ext) {
+			filtered = append(filtered, file)
+		}
+	}
+
+	return filtered
 }
